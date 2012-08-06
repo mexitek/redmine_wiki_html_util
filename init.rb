5eed2b9e448d1df1e8371b3b4ca2f30e33f99d45
Arlo Carreon <arlo.carreon@gmail.com>
2012-08-05 22:29:51 -0300

    Took back the js() macro changes.
    Cannot replace all [ with { because JS will need [ for arrays.

--- init.rb
+++ init.rb
@@ -44,11 +44,7 @@ Redmine::Plugin.register :redmine_gist do
 	Redmine::WikiFormatting::Macros.register do
     desc "Embed raw js"
     macro :js do |obj, args|
-        result = args[0]
-        result = result.gsub(/\[/,'{')
-	result = result.gsub(/\]/,'}')
-	result = result.gsub(/<\/?[^>]*>/, "")
-	result = "<script>"+result+"</script>"
+	result = "<script>"+args[0]+"</script>"
         result
     end	
   end
